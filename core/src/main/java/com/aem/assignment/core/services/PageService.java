944dbc4cc1e4e5e891876af024b183eb35086aca
gargshivam2034 <108911698+gargshivam2034@users.noreply.github.com>
2024-05-28 17:56:16 +0530

    Update PageService.java

--- core/src/main/java/com/aem/assignment/core/services/PageService.java
+++ core/src/main/java/com/aem/assignment/core/services/PageService.java
@@ -4,11 +4,7 @@ import com.aem.assignment.core.entities.ProductEntity;
 
 public interface PageService {
 
-    /**
-     * Creates a page based on the provided product entity.
-     *
-     * @param productEntity The product entity to use for creating the page.
-     */
+   
     void createPage(ProductEntity productEntity);
 
 }
